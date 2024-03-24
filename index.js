53d5bcfdeafb7e7088f1c4786ee7409bd41674b7
lim0n3 <simonestaffa96@gmail.com>
2024-03-24 22:25:21 +0100

    polyfills

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+Object.assign(window, {
+  addEventListener: () => 0,
+  removeEventListener: () => {},
+  dispatchEvent: () => true,
+  CustomEvent: class CustomEvent {},
+});
 import "fast-text-encoding";
 import "react-native-get-random-values";
 import "@ethersproject/shims";
