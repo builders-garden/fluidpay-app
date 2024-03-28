88ca848b8d150bd39d628426209b51e7f9352887
lim0n3 <simonestaffa96@gmail.com>
2024-03-28 12:22:47 +0200

    update request gateway

--- lib/request-network/index.ts
+++ lib/request-network/index.ts
@@ -5,7 +5,7 @@ export const getRequestClient = (provider: any) => {
   const web3SignatureProvider = new Web3SignatureProvider(provider);
   return new RequestNetwork({
     nodeConnectionConfig: {
-      baseURL: "https://xdai.gateway.request.network", //all the requests are stored on gnosis chain and can be retrieved using this public Request node gateway
+      baseURL: "https://gnosis.gateway.request.network", //all the requests are stored on gnosis chain and can be retrieved using this public Request node gateway
     },
     signatureProvider: web3SignatureProvider,
   });
