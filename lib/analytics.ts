cc21c1dae43e43f977a06cd996a5fdeab5c9926b
GiFTED <gkenny896@gmail.com>
2024-04-23 11:02:03 +0100

    type check params in analytics

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,7 +1,10 @@
 import firebaseAnalytics from "@react-native-firebase/analytics";
 
 // custom analytics helper for logging events in firebase
-export const analytics = async (eventName: string, params?: any) => {
+export const analytics = async (
+  eventName: string,
+  params?: Record<string, any>
+) => {
   try {
     await firebaseAnalytics().logEvent(eventName, params);
   } catch (err: any) {
