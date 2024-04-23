3458e49a7d82bfdd1fb3339e71ea00ba8bc03b09
GiFTED <gkenny896@gmail.com>
2024-04-23 14:48:15 +0100

    wip: crashlytics impl

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,4 +1,5 @@
 import firebaseAnalytics from "@react-native-firebase/analytics";
+import crashlytics from "@react-native-firebase/crashlytics";
 
 // custom analytics helper for logging events in firebase
 export const analytics = async (
@@ -9,5 +10,12 @@ export const analytics = async (
     await firebaseAnalytics().logEvent(eventName, params);
   } catch (err: any) {
     console.error(`ERROR_CALLING_${eventName}`, err);
+    logError(`ERROR_CALLING_${eventName}`, err);
   }
+};
+
+// custom crashlytics helper for logging errors in firebase
+export const logError = (message: string, error: Error) => {
+  crashlytics().recordError(error);
+  crashlytics().log(message);
 };
